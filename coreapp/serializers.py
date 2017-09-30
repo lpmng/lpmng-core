cefeb25b38af5f34c376bb9f3d487a09ae3da5b4
Elio Maisonneuve <elio.maisonneuve@eisti.fr>
2017-09-30 23:19:59 +0200

    add password when creating user

--- coreapp/serializers.py
+++ coreapp/serializers.py
@@ -1,3 +1,6 @@
+import hashlib
+from base64 import b64encode
+
 from rest_framework import serializers
 from coreapp.models import LdapUser
 
@@ -5,4 +8,15 @@ from coreapp.models import LdapUser
 class LdapUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = LdapUser
-        fields = ('uid', 'commonname', 'surname', 'mail', 'tel')
+        fields = ('uid', 'commonname', 'surname', 'mail', 'tel', 'password')
+        extra_kwargs = {
+            'password': {'write_only': True}  # Allow to set pwd, but disallow getting the hash from LDAP
+        }
+
+    def create(self, validated_data):
+        password = validated_data.pop('password')  # Get the plain-text pwd and remove it from data to save
+        hashed = hashlib.sha1(password.encode('utf-8')).digest()
+        hashed_string = "{SHA}" + b64encode(hashed).decode()    # Password string formated for LDAP client
+
+        # Create user with the validated_data dict along with the hashed pwd
+        return LdapUser.objects.create(password=hashed_string, **validated_data)
