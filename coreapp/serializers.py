a71b0570a36d8c88e59eedb54027c795017a199f
Elio Maisonneuve <elio.maisonneuve@eisti.fr>
2017-10-11 15:20:37 +0200

    add possibility to change password of user

--- coreapp/serializers.py
+++ coreapp/serializers.py
@@ -1,8 +1,6 @@
-import hashlib
-from base64 import b64encode
-
 from rest_framework import serializers
 from coreapp.models import LdapUser
+from coreapp.utils import hash_password
 
 
 class LdapUserSerializer(serializers.ModelSerializer):
@@ -15,8 +13,15 @@ class LdapUserSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         password = validated_data.pop('password')  # Get the plain-text pwd and remove it from data to save
-        hashed = hashlib.sha1(password.encode('utf-8')).digest()
-        hashed_string = "{SHA}" + b64encode(hashed).decode()    # Password string formated for LDAP client
 
         # Create user with the validated_data dict along with the hashed pwd
-        return LdapUser.objects.create(password=hashed_string, **validated_data)
+        return LdapUser.objects.create(password=hash_password(password), **validated_data)
+
+    def update(self, instance, validated_data):
+        validated_data['password'] = hash_password(validated_data['password'])
+
+        for attr_name,attr_value in validated_data.items():
+            setattr(instance, attr_name, attr_value)
+
+        instance.save()
+        return instance
